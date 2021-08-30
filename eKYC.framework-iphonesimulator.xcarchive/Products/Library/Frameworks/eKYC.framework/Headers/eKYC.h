de598e8114e89fe825442da2783f60269511a0b9
Mahdi Mahjoobi <mahdi.m@email.com>
2021-08-30 07:50:30 +0430

    Added xcarchive file

---
+++ eKYC.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/eKYC.framework/Headers/eKYC.h
@@ -0,0 +1,20 @@
+//
+//  eKYC.h
+//  eKYC
+//
+//  Created by Mahdi Mahjoobi on 7/27/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "SwiftyRSA.h"
+#import "NSData+SHA.h"
+
+//! Project version number for eKYC.
+FOUNDATION_EXPORT double eKYCVersionNumber;
+
+//! Project version string for eKYC.
+FOUNDATION_EXPORT const unsigned char eKYCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <eKYC/PublicHeader.h>
+
+
